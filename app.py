99a84f74770d52db7b98ccdbefef242e4a703c39
srujan <srujansripathi29@gmail.com>
2026-04-14 14:46:36 +0530

    triggering cloud build

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    return "Hello,World from Cloud Build"
+    return "Hello World from Cloud Build"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
